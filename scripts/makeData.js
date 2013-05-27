ff886a83d749c7e2e6dd969903f2a1e14b7010f3
Nick Yahnke <nthitz@gmail.com>
2013-05-26 23:22:56 -0700

    basic parsing

---
+++ scripts/makeData.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var _ = require('underscore');
+var dirs = ['africa','america','emed','europe','seasia','wpacific'];
+var dir = null;
+var dirIndex = 0;
+var numFolderOn = 0;
+var dataPath = "data/";
+var lists = [
+	{path:"countries", attr:"country"},
+	{path:"ages",attr:"age"},
+	{path:"femaleObese",attr:"fObese"},
+	{path:"femaleOverweight", attr:"fOver"},
+	{path:"maleObese", attr:"mObese"},
+	{path:"maleOverweight",attr:"mOver"},
+	{path:"samplesizes",attr:"size"},
+	{path:"years",attr:"years"}
+]
+var dataToLoad = "null";
+var countries = [];
+loadFolder()
+function loadFolder() {
+	console.log(dirIndex);
+	dir = dirs[dirIndex];
+	if(typeof dir === 'undefined') {
+
+		allCountriesLoaded();
+		return;
+	}
+	dataToLoad = [];
+	_.each(lists, function(list) {
+		dataToLoad.push(_.extend({},list));
+	})
+		//console.log(dataToLoad);
+	loadNextData(dir);
+}
+function allCountriesLoaded() {
+	_.each(countries, function(country) {
+	//	console.log(country.country);
+	})
+	console.log(countries);
+}
+function loadNextData(dir) {
+	var numData = dataToLoad.length;
+	for(var i = 0 ; i < numData; i++) {
+		//console.log(list);
+		var list = dataToLoad[i];
+		 if(typeof list['data'] === 'undefined') {
+			var listPath = dataPath+dir+"/"+list['path'] + '.list';
+			console.log(listPath);
+			fs.readFile(listPath, 'utf8', loadList)
+			return;
+		}
+	}
+	dataLoaded();
+}
+function dataLoaded() {
+	var dataObjects = [];
+	_.each(dataToLoad[0].data,function(data) {
+		dataObjects.push({})
+	})
+	_.each(dataToLoad, function(list, listIndex) {
+		_.each(list.data, function(dataVal, dataIndex) {
+			dataObjects[dataIndex][list.attr] = dataVal;
+		})
+	})
+	//console.log(dataObjects);
+	_.each(dataObjects, function(data) {
+		data.region = dirs[dirIndex];
+		countries.push(data);
+	})
+	dirIndex++;
+	loadFolder();
+}
+function loadList(err, listData) {
+	if(err !== null) {
+		console.log(err);
+		return;
+	}
+	//console.log(listData);
+	var numData = dataToLoad.length;
+	for(var i = 0 ; i < numData; i++) {
+		var list = dataToLoad[i];
+		if(typeof list['data'] === 'undefined') {
+			list['data'] = listData.split("\n");
+			break;
+		}
+	}
+
+	loadNextData(dir);		
+
+}
